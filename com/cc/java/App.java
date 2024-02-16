ed446dbe1806b279f5a4e4de5f7a46585b46cd90
Chris <159909987+ChrizzlyBe4r@users.noreply.github.com>
2024-02-16 14:19:18 +0000

    0.1

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,24 +1,25 @@
 package com.cc.java;
 
 public class App {
-    
-    public static void main(String[] args) 
-    {
-        
-        // Variablen deklarieren
-
+    public static void main(String[] args) {
+        // Hier wird ausgegeben
+        System.out.println("FML");
+                
         String food = "noch kein Burger gewählt";
-        String drink = "noch kein Getränk gewählt";
+        String drink = "noch kein Drink gewählt";
         String itemNumStr;
         String flag;
+        
+        System.out.println(food);
+        System.out.println(drink);
 
-        System.out.println(food);  
-        System.out.println(drink); 
-
-  
-
-
+        //Bestelldialog
+        System.out.println("---------------");
+        System.out.println("Guten Tag");
+        System.out.println("Auswahl: 1 - 2 - 3");
+        
+        //Eingabe
+        itemNumStr = System.console().readLine("Ihre Wahl?:");
+        System.out.println(itemNumStr);
     }
-
 }
-
