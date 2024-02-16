5e2c4b10d35c2913e8b1b644d14713f29f0ac923
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-02-16 14:08:07 +0000

    Bestelldialog 1  / Ein- und Ausgabe

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -15,7 +15,14 @@ public class App {
         System.out.println(food);  
         System.out.println(drink); 
 
-  
+        // Bestelldialog 1 : Ausgabe
+        System.out.println("-------------------------------");
+        System.out.println("Guten Tag bei MD, Ihre Bestellung bitte!");
+        System.out.println("Hamburger(1), Cheeseburger(2) oder Chilliburger(3) ?");
+
+        // Bestelldialog 1 : Eingabe
+        itemNumStr = System.console().readLine("Ihre Wahl? : ");
+        System.out.println(itemNumStr);
 
 
     }
