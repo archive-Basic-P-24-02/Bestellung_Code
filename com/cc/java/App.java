04faa83a7a0245cbe7746a22dd8985523aa0452a
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-02-16 11:57:54 +0000

    Variablen deklariert

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -2,10 +2,19 @@ package com.cc.java;
 
 public class App {
     
-    public static void main(String[] args) {
-
-        System.out.println("Hello, World!");
+    public static void main(String[] args) 
+    {
+        
+        // Variablen deklarieren
+        String food = "leer";
+        String drink = "leer";
        
+        System.out.println("Hallo Christoph!");  
+        System.out.println(food);  
+        System.out.println(drink); 
+
+
     }
+
 }
 
